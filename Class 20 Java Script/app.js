fd722e0cf55215131f629c9f1657efaf73466164
Muhammad Hamza <139335479+mhamza813@users.noreply.github.com>
2024-07-30 14:40:54 +0500

    Java Script Start
    Java Script

---
+++ Class 20 Java Script/app.js
@@ -0,0 +1,97 @@
+// alert("hello world");
+// document.write("hello world");
+// console.log("hello world")
+
+// var a = 5;
+// var b =5;
+// var c =a+b;
+// alert(c)
+
+// var a =205;
+// var b =4.5;
+// var c =a+b;
+// alert(c)
+
+// var a = "Hamza";
+// var a = "Zaid";
+// alert(a)
+
+// var a =15;
+// var b=a+a+15;
+// alert(b)
+
+// var a = "zaid";
+// var b =10;
+// var c = a+b;
+// alert(c)
+
+// var a = "zaid";
+// var b =10;
+// var c = a-b;
+// alert(c)
+
+// var a=25;
+// var b=15;
+// var c=a-b;
+// alert(c)
+
+// var a=25;
+// var b=115;
+// var c=a-b;
+// alert(c)
+
+// var a=125
+// var b=5;
+// var c=a/b;
+// alert(c)
+
+// var a=5+8-10*25/8
+// alert(a)
+
+// var a=2+(5-3)*8/125
+// alert(a)
+
+// var a=(((5+3)*8)/2)-14
+// alert(a)
+
+// Increment
+// Post x++ x=x+1 x+=1
+// Pre ++x 
+
+// Decrement
+// Post x--
+// Pre --x
+
+
+// var a = 5;
+// var b = a++;
+// alert(b)
+
+// var a = 5;
+// var b = ++a;
+// alert(b)
+
+// var a = 5;
+// var b = a--;
+// alert(b)
+
+// var a = 5;
+// var b = --a;
+// alert(b)
+
+// var a = 3;
+// var b = a + ++a - --a + a++ - --a + ++a + a++ + a--
+// alert(b)
+
+// var a =  5;
+// var b = a++ - ++a + a-- + --a  + --a - a-- + a-- + ++a ;
+// alert(a)
+
+// var a = prompt("Enter your name?");
+// alert(a)
+
+// var a = +prompt("Enter your name?");
+// alert(a)
+
+var a = +prompt("Enter your name?");
+alert(a+10)
