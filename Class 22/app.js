d83de5252c28ed70b6fb2ccf93280098439f6770
Muhammad Hamza <139335479+mhamza813@users.noreply.github.com>
2024-07-30 21:35:30 +0500

    Java
    JavaScript

---
+++ Class 22/app.js
@@ -0,0 +1,410 @@
+                            //      CHP # 01 (ALERT)
+
+// var a = prompt("Enter your First Name");
+// var b = prompt("Enter your Last Name");
+// var c = prompt("Enter your Email");
+// var d = prompt("Phone Number");
+// var e = prompt("Enter your Password");
+// var f = a+b+c+d+e
+// alert(f)
+
+
+// var a = ("You're LEARNING JavaScript")
+// alert(a)
+
+
+// var a = ("I Like JavaScript");
+// alert(a)
+
+                                //  CHP # 02 (Variablesfor String)
+
+// var a = "Muhammad";
+// var a = "Hamza";
+// var Name = "Declare a Variable";
+// var teamname = "Hell King";
+// alert(teamname);
+// var bestMan = "Charlie";
+// bestMan = "John"
+// alert(bestMan);
+
+                                //  CHP # 03 (Variables for numbers)
+
+// var caseQty = 144;
+// var num = 9;
+// var total = caseQty + num;
+// alert(total);
+// var totala =total + 2;
+// alert(totala);
+
+                                //  CHP # 04 (Variable names Legal and Illegal)
+
+// var productcost = 3.45;
+// var Nameofband;
+// var assign_number = 5; 
+// var a = "Muhammad";
+// var a = "Hamza";
+
+// Legal Variables:
+
+// productcost
+// Nameofband
+// assign_number
+// a
+
+// Illegal Variables:
+
+// This code is not used to Illegal Variables 
+
+                                // CHP # 05 (Math Expression I)
+
+// var num = 20 % 6;
+// var largeNum = 1000*2000;
+// var a = 45-5/5;
+// alert(a);
+// alert(largeNum);
+// alert(num);
+
+                              //   CHP # 06 (Math Expression II)
+
+// var x = 50;
+// x = x + 1;
+// var y = x++;
+// var y = 50;
+// var z = --y;
+// var zz = 49+1
+// var zzz = ++zz
+// alert(zzz);
+
+                                // CHP # 07 (Math Expression III)
+
+// var calculatedNum = 2 + (2 * 6);
+// alert(calculatedNum);
+// var calculatedNum = (2 + 2) * 6;
+// alert(calculatedNum);
+// var calculatedNum = (2 + 2) * (4 + 2);
+// alert(calculatedNum);
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// alert(calculatedNum);
+// var a = (2 + 2) * (4 + 10);
+// alert(a);
+// var b = 2 + (2 * 4) + 10;
+// alert(b);
+// var c = 4 / (2 * 4);
+// alert(c);
+
+                                // CHP # 08 (Concatenating Text Strings)
+
+// var num = "2" + "2";
+// alert(num);
+// message = ("Hello," + "Dolly");
+// alert(message);
+// alert("33" + 3);
+// var a = "Pakistan";
+// var b = "Zindabad";
+// var c = a + " " + b;
+// alert(c);
+// var d = "The number is " + 15;
+// alert(d);
+// var e = "Muhammad";
+// var f = "Hamza";
+// var g = e + " " + f;
+// alert(g);
+
+                              // CHP # 09 (Prompts)
+
+// var a = prompt("Enter your First Name?");
+// var b = prompt("Country?", "China");
+// var yourName = prompt("Enter Your Name?");
+// var c = prompt("Please enter your city:", "New York");
+// var d = ("Select the Next Button");
+// alert(d);
+// var e = ("Are you frpm:", "Pakistan");
+// alert(e);
+// var f = prompt("Please give me your response");
+// alert(f);
+
+                             // CHP # 10 (if statments)
+
+// var city = "Karachi";
+
+// if (city === "Karachi") {
+//     console.log("The City of Lights");
+// } else {
+//     console.log("Not the City of Lights");
+// }
+
+// var x =  5;
+// var y = 5;
+
+// if (x === y) {
+//     var z = prompt("Enter your z Value");
+//     alert(z);
+// }else{
+//     alert("I assign you another variable is 3");
+// }
+
+// var zipcode = prompt("Enter your Zipcode?");
+// if (ZipCode === 10010){
+//     alert("Karachi");
+// } else {
+//     alert("Please write Correct City");
+// }
+
+// var x = 1;
+// if(x ===1) {
+//     x = 2;
+//     alert("New Value of x is: " + x);
+// } else {
+//     alert("Do not x value is 1");
+// }
+
+                               // CHP # 11 (Comparison Operators)
+
+// var x = 1;
+// var y = 2;
+// if(x != y){
+//      alert("true");
+// } else {
+//     alert("false");
+// }
+
+
+// var x = 5;
+// var y = 100;
+// if(x => y){
+//     alert("true");
+// } else {
+//     alert("false");
+// }
+
+
+// var x = 4;
+// var y =10;
+// if(x != y){
+//     alert("Congratulations");
+// } else {
+//     alert("Sorry");
+// }
+
+
+// var a = prompt("What is your first name?");
+// var b = "hamza";
+// if(a !== b){
+//     alert("No Match");
+// } else {
+//     alert("It will be");
+// }
+
+                                  // CHP # 12 (if…else and else if statements)
+
+// var x = 5;
+// var y = 50;
+// var z = prompt("Enter the digit?");
+// if (x >= y){
+//     alert("Display");
+// } else {
+//     alert("Not Display");
+// }
+
+
+
+// var perc = +prompt("Enter your Percentage");
+// if (perc >= 80 && perc <= 100) {
+//     document.write("A+");
+//     } else if (perc >= 70 && perc < 79){
+//         document.write("A")
+//     } else if (perc >= 60 && perc < 69){
+//         document.write("B")
+//     } else if (perc >= 50 && perc < 59){
+//         document.write("C")
+//     } else if (perc >= 40 && perc < 49){
+//         document.write("D")
+//     } else if (perc >= 33 && perc < 39){
+//         document.write("E")
+//     } else {
+//         (perc >= 0 && perc < 32)
+//         document.write("Fail")
+//     } 
+
+
+
+// var city = prompt("Enter your City Name?")
+// if (city === "Karchi"){
+//     alert("Acknowledging it is Karachi")
+// } else if(city === "Lahore"){
+//     alert("Acknowledging it is Lahore")
+// } else {
+//     alert("It's a Different City")
+// }
+
+                                 // CHP # 13 (Testing sets of Conditions)
+
+// var a = 1;
+// var b = 2;
+// var c = 3;
+// var d = d;
+// if (a===b && c===d){
+//     alert("True")
+// } else {
+//     alert("False")
+// }
+
+
+
+// if (a===b || c!=d){
+
+// }
+
+
+
+// var a = "Hamza";
+// var b = "Arsalan";
+// var age = 60;
+// if ((age <=60) && (a ==="Hamza" || b === "Arsalan")){
+//     alert("Meet")
+// } else {
+//     alert("Cannot Meet")
+// }
+
+
+// var a = 5;
+// var b = 6;
+// if (a < b || b > a){
+//     alert("True")
+// } else{
+//     alert("False")
+// }
+
+
+
+// var myfirstname = "Muhammad"
+// var mylastname = "Hamza"
+// var firstname = prompt("Enter your First Name: ")
+// var lastname = prompt("Enter your Last Name: ")
+// if (firstname === myfirstname && lastname===mylastname){
+//     alert("Match")
+// } else{
+//     alert("No Match")
+// }
+
+
+                                // CHP # 14 (If statements nested)
+
+
+// var pass = +prompt("Enter your Password");
+// if (pass <= 5){
+//     alert("Password must be greater than 5")
+// } else {
+//     alert("OK")
+// }
+
+
+// var a = 1;
+// var c = 2;
+// if (a === 1){
+//     alert('One ok')
+// } else if (c === 2) {
+//     alert("MAX");
+//     } else {
+//         alert("OK")
+//     }
+
+
+
+// var a = 1;
+// var c = "Max";
+// if (a === 1 && c === "Max")  {
+ 
+//     alert("OK");
+//     }
+
+
+
+// var a = 5;
+// var b = 5;
+// if (a===b && b <= a){
+//     alert("It Will")
+// }    
+
+    
+                                // CHP # 15 (Array I)
+
+
+// var myArray = [];
+// var singleElementArray = ["Hello"];
+// var alphabet = ["h", "i", "j", "k"];
+// alert(alphabet[2]); 
+// var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+// var length = alphabet.length;
+// alert(length);
+// var myArray = ["First Element"]; 
+// myArray[1] = "Second Element"; 
+// alert(myArray[1]); 
+
+
+
+                                // CHP # 16 (Array II)
+
+
+// var Alphabet = ["h", "i", "j", "k"];
+// Alphabet.pop(); 
+// document.write(Alphabet); 
+
+
+
+// var Alphabet = ["h", "i", "j", "k"];
+// Alphabet.push(5);
+// document.write(Alphabet);
+
+
+// var Alphabet = ["h", "i", "j", "k"];
+// Alphabet.push(5); 
+// document.write(Alphabet); 
+
+
+                              // CHP # 16 (Array III)
+
+                              
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.shift();
+// document.write(sizes); 
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.unshift(1, 2, 3);
+// document.write(sizes); 
+
+
+// var myArray = ["Original"];
+// myArray.unshift("New First");
+// alert(myArray[0]); 
+
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.splice(2, 0, "L");
+// document.write(sizes); 
+
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// var regSizes = sizes.slice(0, 3);
+// document.write(regSizes); 
+
+
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// pets.splice(1, 0, "rabbit", "hamster");
+// pets.splice(2, 3);
+// document.write(pets);
+
+
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// pets.splice(1, 2);
+// document.write(pets); 
+
+
+// var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+// var reducedPets = pets.slice(3, 5);
+// document.write(reducedPets);
+
+
+
+
+    
