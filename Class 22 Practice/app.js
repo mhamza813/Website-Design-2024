16bef31beb983e399cd5fb28b9c9b61bb7592fa0
Muhammad Hamza <139335479+mhamza813@users.noreply.github.com>
2024-08-05 18:38:59 +0500

    JavaScript
    JavaScript

---
+++ Class 22 Practice/app.js
@@ -0,0 +1,50 @@
+
+                    // Show
+// var arr = ['Ali','Zaid','Memoona']
+// document.write(arr [2]) 
+
+// var arr = ['Ali','Zaid','Memoona']
+// document.write(typeof(arr)) 
+
+// var arr ="aLI"
+// document.write(typeof(arr))
+                                               
+                            //  Add End
+// var arr = ['Ali','Zaid','Memoona']
+// arr.push('Cow');
+// document.write(arr);
+ 
+// var arr = ['Ali','Zaid','Memoona']
+// arr.push('Hamza','Talha');
+// document.write(arr);
+
+                            //    Delete End
+// var arr = ['Ali','Zaid','Memoona']
+// arr.pop();
+// document.write(arr);
+
+                                //  Delete First
+// var arr = ['Ali','Zaid','Memoona']
+// arr.shift();
+// document.write(arr);
+
+                                // Add First
+// var arr = ['Ali','Zaid','Memoona']
+// arr.unshift('Hamza');
+// document.write(arr);
+
+                                //   Add Mid Delete + Add 
+// var arr = ['Ali','Zaid','Memoona']
+// arr.splice(0,1,'Hamza');
+// document.write(arr);
+                       
+// var arr = ['Ali','Zaid','Memoona']
+// arr.splice(1,2,'Hamza','Talha');
+// document.write(arr);
+
+                // Choice your show Variable
+
+// var arr = ['Ali','Zaid','Memoona','Hamza','Talha','Khawla','Shehzadi'];
+// var regarr = arr.slice(2,4);
+// document.write(regarr); 
+
