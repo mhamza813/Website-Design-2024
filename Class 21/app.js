e2d264898be50125fed88cf9cb7c3f845086a0ee
Muhammad Hamza <139335479+mhamza813@users.noreply.github.com>
2024-08-20 14:14:18 +0500

    JavaScript
    JavaScript

---
+++ Class 21/app.js
@@ -0,0 +1,24 @@
+// alert("Hello, How r u \n Where r u from");
+
+// console.log("Hey How r u");
+
+// let age= 15;
+// alert(age);
+
+// document.write(2003);
+
+// birthdayyear = 2003;
+// document.write(birthdayyear)
+
+// let message="Hello World";
+// alert(message)
+
+// var a=2;
+// var b=1;
+// var result= -a - --b + ++b + b--;
+// alert(result);
+
+// let name= +prompt("What is your name?")
+// alert(name)
+
+
